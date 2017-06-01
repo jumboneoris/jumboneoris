2db52c6210ac5936b2b3d9441f8162d90103ca6c
drosso <damian.rosso@neoris.com>
2017-06-01 15:07:25 -0300

    fix gitignore

---
+++ nopCommerce_3.90_Source/Backup/Plugins/Nop.Plugin.Widgets.HelloWorld/Domain/HelloWorldRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Web.Framework;
+
+namespace Nop.Plugin.Widgets.HelloWorld.Domain
+{
+    public partial class HelloWorldRecord : BaseEntity
+    {
+        [NopResourceDisplayName("Plugins.Widgets.HelloWorld.Message")]
+        public string Message { get; set; }
+        [NopResourceDisplayName("Plugins.Widgets.HelloWorld.UrlImg")]
+        public string UrlImg { get; set; }
+    }
+}
---
+++ nopCommerce_3.90_Source/Backup/Plugins/Nop.Plugin.Widgets.HelloWorld/Domain/HelloWorldSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Widgets.HelloWorld.Domain
+{
+    public partial class HelloWorldSettings : ISettings
+    {
+        public string Message { get; set; }
+        public string UrlImg { get; set; }
+    }
+}
